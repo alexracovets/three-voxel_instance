be000b1be1c91036741c2e94648f7a0789229f12
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-02-26 08:42:45 +0200

    test mobile

--- src/Components/MeScene.jsx
+++ src/Components/MeScene.jsx
@@ -19,7 +19,7 @@ function MeScene() {
             <ambientLight intensity={1} />
             <spotLight position={[5, 10, 7]} intensity={50} />
             <PerspectiveCamera makeDefault position={[15, -10, 20]} />
-            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false}  minZoom={4}/>
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} enableDamping={false} enableRotate={false} />
             <Scroll models={modelURLs} currentScene={currentScene} setCurrentScene={setCurrentScene} />
             <Plane args={[100, 100]} position={[0, -3.2, 0]} rotation={[-Math.PI / 2, 0, 0]} castShadow receiveShadow>
                 <meshStandardMaterial color={'gray'} receiveShadow />
